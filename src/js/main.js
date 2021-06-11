37a93c9ea6f2fea1fd8f3d925e3ceb19bc940ec7
EvgeniyTurovets <websafrasoft@gmail.com>
2021-06-11 17:30:03 +0300

    1
    1

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+(function() {
+    const swiper = new Swiper('.first-sec-slider', {
+        // Optional parameters
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next2',
+          prevEl: '.swiper-button-prev2',
+        },
+    });
+})();
