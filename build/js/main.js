f4418410736f078c970bc2894c52d84615477603
EvgeniyTurovets <websafrasoft@gmail.com>
2021-06-15 13:10:46 +0300

    1
    1

--- build/js/main.js
+++ build/js/main.js
@@ -5,15 +5,10 @@
           nextEl: '.swiper-button-next2',
           prevEl: '.swiper-button-prev2',
         },
-
-        on: {
-          init: function (event) {
-           this.$el[0].querySelector('.swiper-count').querySelector('.max').textContent = event.slides.length
-          },
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
         },
-    });
-    swiper.on('slideChange', function (event) {
-      this.$el[0].querySelector('.swiper-count').querySelector('.now').textContent = event.realIndex + 1
     });
     
     // меню 
@@ -49,15 +44,17 @@
     })
    
     
-
-
-
+    // .swiper-section
     let sliderCount = document.querySelectorAll('.swiper-section')
     let options = {
         init: false,
         navigation: {
               nextEl: '.swiper-button-next2',
               prevEl: '.swiper-button-prev2',
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
         },
         spaceBetween: 23,
         lazy: true,
@@ -74,20 +71,74 @@
             },
             
         },
-        on: {
-            init: function (event) {
-             this.$el[0].querySelector('.swiper-count').querySelector('.max').textContent = event.slides.length
-            },
-        },
     }
     for(let i = 0; i < sliderCount.length; i++){
         mySwiper = new Swiper(sliderCount[i], options);
         mySwiper.init();
-
-        mySwiper.on('slideChange', function (event) {
-            this.$el[0].querySelector('.swiper-count').querySelector('.now').textContent = event.realIndex + 1
-        });
     }
+
+    // .swiper-section2
+    let sliderCount2 = document.querySelectorAll('.swiper-section2')
+    let options2 = {
+      init: false,
+      navigation: {
+            nextEl: '.swiper-button-next2',
+            prevEl: '.swiper-button-prev2',
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      spaceBetween: 23,
+      lazy: true,
+      slidesPerView: 1,
+      breakpoints: {
+          // when window width is >= 320px
+          560: {
+              slidesPerView: 2,
+              spaceBetween: 23
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 23
+          },
+          1200: {
+            slidesPerView: 4,
+            spaceBetween: 23
+          },
+          
+      },
+    }
+    for(let i = 0; i < sliderCount.length; i++){
+      mySwiper2 = new Swiper(sliderCount2[i], options2);
+      mySwiper2.init();
+    }
+
+    // сладер брендов
+    const swiperBrand = new Swiper('.brand-slider', {
+        navigation: {
+          nextEl: '.swiper-button-next2',
+          prevEl: '.swiper-button-prev2',
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+        },
+        spaceBetween: 40,
+        slidesPerView: 2,
+        lazy: true,
+        breakpoints: {
+            768: {
+                spaceBetween: 40,
+                 slidesPerView: 4,
+            },
+            1024: {
+                spaceBetween: 80,
+                 slidesPerView: 6,
+            },
+        }
+    });
+
     // табы на главной всегда нужно после слайдеров запускать а то ломаются
     let tabsNavs = document.querySelectorAll('.tabs-nav')
     tabsNavs.forEach(function(el,index){
@@ -101,6 +152,7 @@
 
     tabsNavs.forEach(nav => nav.addEventListener('click', () =>{
         if(!nav.classList.contains('active')){
+
             thisTabsNav = nav.closest('.tabs').querySelectorAll('.tabs-nav')
             thisTabsNav.forEach(function(el, index) {
                 el.classList.remove('active')
@@ -115,6 +167,7 @@
 
             thisAttr = nav.getAttribute('data-index')
             tabsItems[thisAttr].classList.add('active')
+
         }
     }))
 })();
