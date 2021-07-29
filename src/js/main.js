b05919b5491ad7fda8e04873ca99324b8a96d279
EvgeniyTurovets <websafrasoft@gmail.com>
2021-07-29 15:29:07 +0300

    1
    1

--- src/js/main.js
+++ src/js/main.js
@@ -44,18 +44,9 @@
             thisMain = 1;
         }
 
-        if(window.innerWidth <= 992){
-          
-            if(window.scrollY > 0){
-                header.classList.add('sroll')
-                header.classList.remove('main')
-            }else{
-                header.classList.remove('sroll')
-                if(thisMain){
-                    header.classList.add('main')
-                }
-            }
-
+        if(window.scrollY > 0){
+            header.classList.add('sroll')
+            header.classList.remove('main')
         }else{
             header.classList.remove('sroll')
             if(thisMain){
